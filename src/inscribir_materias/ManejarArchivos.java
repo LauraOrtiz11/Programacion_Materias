6d12d63157fd1ec429a72fa941140d86c6bc9e68
Nicolas Gonzalez <nicolasf.gonzalez1305@gmail.com>
2024-05-16 17:09:44 -0500

    Prueba

--- src/inscribir_materias/ManejarArchivos.java
+++ src/inscribir_materias/ManejarArchivos.java
@@ -7,7 +7,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
 public class ManejarArchivos {
 
